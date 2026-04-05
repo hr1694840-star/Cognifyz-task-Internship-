775cda6b458322f3ad6bc47c04caf669246ad781
Harsh Rai <hr1694840@gmail.com>
2026-04-05 15:49:09 +0530

    Add files via upload

---
+++ level--1.py
@@ -0,0 +1,11 @@
+# level-1>
+# Task--(String Reversal)
+# create a python function that takes a string as input and return the reverse of string. 
+# For example, if the input is "hello" the function should return "olleh".
+
+def reverse_string(s):
+   # Example: "hello" => "olleh"
+    return s[::-1]
+print("String Reversal")
+print(reverse_string("hello"))  # olleh
+print("------")
